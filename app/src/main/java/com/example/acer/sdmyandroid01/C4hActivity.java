daa6197ba5c063691bfa13c3e662abf6f67a1eeb
170507180119 <2916076379@qq.com>
2020-04-02 09:57:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/acer/sdmyandroid01/C4hActivity.java
@@ -0,0 +1,16 @@
+package com.example.acer.sdmyandroid01;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class C4hActivity extends AppCompatActivity {
+                    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
